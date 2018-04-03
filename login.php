54c69a200f88e49a452a037194823ded73086774
Manish <37529671+manish-fn@users.noreply.github.com>
2018-04-03 17:23:31 +0530

    Add files via upload
    test

---
+++ login.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>login form</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body> 
+      <div class="login">
+	  <img src="login.png" class="avatar">
+	  <h1>Login Here</h1>
+	  <form action="login2.php" method="POST">
+	  <p>username</p>
+	  <input type="text" name="email" placeholder="Enter the e-mail"/>
+	  <p>password</p>
+	  <input type="password" name="password" placeholder="Enter the Password"/>
+	  <input type="submit" name="submit" value="login"/>
+	  <a href="#">Forget Password</a><br>
+	  <a href="#">Register</a>
+	  </form>
+	  
+	  </div>
+</body>
+</html>
